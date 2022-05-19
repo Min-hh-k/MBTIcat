9bd5cbb002419f617c2b482f5ae7370143330d35
Minhhk <alszn2222@gmail.com>
2022-05-19 12:49:21 +0900

    add result

--- mbticat/src/pages/Question.js
+++ mbticat/src/pages/Question.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 import {ProgressBar, Button} from 'react-bootstrap';
-import { useNavigate } from 'react-router-dom';
+import { createSearchParams, useNavigate } from 'react-router-dom';
 import { QuestionData } from '../assets/data/questiondata';
 
 
@@ -14,7 +14,6 @@ const Question = () => {
     {id: "JP", score: 0},
   ]);
   const navigate = useNavigate();
-  console.log('totalScore',totalScore)
 
   const handleClickButton = (no, type) => {
     const newScore = totalScore.map((s) =>
@@ -25,8 +24,19 @@ const Question = () => {
     if(QuestionData.length !== questionNo + 1) {
       setQuestionNo(questionNo + 1)
     } else {
+      //mbti 도출식
+      const mbti = newScore.reduce(
+        (acc, curr) => 
+        acc + (curr.score >= 2 ? curr.id.substring(0,1) : curr.id.substring(1,2)),""
+      );
+      console.log('mbti',mbti)
       //결과 페이지 이동
-      navigate("/result");
+      navigate({
+        pathname: "/result",
+        search: `?${createSearchParams({
+          mbti: mbti,
+        })}`
+      });
     }
 
 
