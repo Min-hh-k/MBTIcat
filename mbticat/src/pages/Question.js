b105eb12d442647a718fc5cb7febaf7ccb6148b5
Minhhk <alszn2222@gmail.com>
2022-05-19 10:38:25 +0900

    question update

--- mbticat/src/pages/Question.js
+++ mbticat/src/pages/Question.js
@@ -1,9 +1,38 @@
 import React from 'react';
+import styled from 'styled-components';
+import {ProgressBar, Button} from 'react-bootstrap';
+import { QuestionData } from '../assets/data/questiondata';
 
 const Question = () => {
   return (
-    <div>질문 화면</div>
+    <Wrapper>
+        <ProgressBar striped variant="danger" now={80} style={{marginTop: '20px' }}/>
+        <Title>{QuestionData[0].title}</Title>
+        <ButtonGroup>
+          <Button style={{width: "40%", minHeight: "200px", fontSize: "15pt"}}>{QuestionData[0].answera}</Button>
+          <Button style={{width: "40%", minHeight: "200px", fontSize: "15pt", marginLeft:"20px"}}>{QuestionData[0].answerb}</Button>
+        </ButtonGroup>
+
+    </Wrapper>
   )
 }
 
 export default Question;
+
+const Wrapper = styled.div`
+  height: 100vh;
+  width: 100%;
+`
+const Title = styled.div`
+  font-size: 40pt;
+  text-align: center;
+  font-family: "LeeSeoYun"
+`
+
+const ButtonGroup = styled.div`
+display: flex;
+flex-direction: row;
+align-items: center;
+justify-content: center;
+font-family: "LeeSeoYun"
+`
