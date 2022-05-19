3a36d3aad96888eaf1bec3d091e66a113d276d5c
Minhhk <alszn2222@gmail.com>
2022-05-19 10:17:00 +0900

    add router

--- mbticat/src/App.js
+++ mbticat/src/App.js
@@ -1,9 +1,16 @@
 import Home from "./pages/Home";
+import Question from "./pages/Question";
+import Result from "./pages/Result";
+import {Routes, Route} from 'react-router-dom';
+import '../src/assets/';
 
 function App() {
   return (
-    <Home />
-
+  <Routes>
+    <Route path="/" element={<Home />} /> 
+    <Route path="/question" element={<Question />} /> 
+    <Route path="/result" element={<Result />} /> 
+  </Routes>
   );
 }
 
